a22b302a274c99249328fb1b388016a2d368905a
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-02-11 04:23:55 +0300

    Second Task is done

--- yandexpractice/first_steps/main.cpp
+++ yandexpractice/first_steps/main.cpp
@@ -1,12 +1,23 @@
 //Task 1
+// #include <iostream>
+//
+// int main() {
+//     std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n"
+//                  "  - is a better C\n"
+//                  "  - supports data abstraction\n"
+//                  "  - supports object-oriented programming\n"
+//                  "  - supports generic programming.\n";
+//     return 0;
+// }
+
+//Task 2
 #include <iostream>
 
 int main() {
-    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n"
-                 "  - is a better C\n"
-                 "  - supports data abstraction\n"
-                 "  - supports object-oriented programming\n"
-                 "  - supports generic programming.\n";
+    int a, b;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << a+b << std::endl;
     return 0;
 }
 
