0fb2f8319a91f9fa79a05fa37a3bb9beb5f7eac5
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-02-11 03:40:27 +0300

    Second program added

--- yandexpractice/main.cpp
+++ yandexpractice/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout<<"Hello, World!";
+    std::string name;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello, " << name;
     return 0;
 }
+
+// #include <iostream>
+//
+// int main() {
+//     std::cout<<"Hello, World!";
+//     return 0;
+// }
