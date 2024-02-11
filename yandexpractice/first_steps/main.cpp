f10116c1f7a384521eb8e020bc15964ea4a56654
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-02-11 03:53:49 +0300

    First Task is done

--- yandexpractice/first_steps/main.cpp
+++ yandexpractice/first_steps/main.cpp
@@ -1,13 +1,25 @@
+//Task 1
 #include <iostream>
-#include <string>
 
 int main() {
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello, " << name;
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n"
+                 "  - is a better C\n"
+                 "  - supports data abstraction\n"
+                 "  - supports object-oriented programming\n"
+                 "  - supports generic programming.\n";
     return 0;
 }
+
+// #include <iostream>
+// #include <string>
+//
+// int main() {
+//     std::string name;
+//     std::cout << "What is your name?" << std::endl;
+//     std::cin >> name;
+//     std::cout << "Hello, " << name;
+//     return 0;
+// }
 
 // #include <iostream>
 //
