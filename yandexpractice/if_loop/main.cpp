0d676802bab3de9eb7863e2afdf9d9e743325807
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-02-13 23:19:51 +0300

    Task 2 is solved

--- yandexpractice/if_loop/main.cpp
+++ yandexpractice/if_loop/main.cpp
@@ -1,4 +1,5 @@
 // Task 1
+/*
 #include <iostream>
 
 int main() {
@@ -7,6 +8,28 @@ int main() {
     if ((std::abs(coord1-coord3)==std::abs(coord2-coord4)) || coord1==coord3 || coord2==coord4) {
         std::cout<<"YES";
     }else {
+        std::cout<<"NO";
+    }
+    return 0;
+}
+*/
+
+// Task 2
+#include <cmath>
+#include <iostream>
+
+int main() {
+    int a, b, c;
+    std::cin>>a>>b>>c;
+
+    if (!((a + b > c) && (a + c > b) && (b + c > a))) {
+        std::cout<<"UNDEFINED";
+    }
+    else if ((pow(a, 2) + pow(b, 2) == pow(c, 2)) ||
+        (pow(a, 2) + pow(c, 2) == pow(b, 2)) ||
+        pow(b, 2) + pow(c, 2) == pow(a, 2)) {
+        std::cout<<"YES";
+    } else {
         std::cout<<"NO";
     }
     return 0;
